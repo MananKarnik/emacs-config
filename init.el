9b0723032e6e356b97cfdf83255fe5a6fa1d6270
Manan Karnik <manankarnik03@gmail.com>
2022-04-29 18:00:26 +0530

    Removed comment

--- init.el
+++ init.el
@@ -162,14 +162,6 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-;(use-package evil-magit
-;  :after magit)
-
-
-
-
-
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
