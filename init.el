00b961bdb901c8f52ceb46ad238c42978a8a3405
Manan Karnik <manankarnik03@gmail.com>
2022-04-30 21:55:41 +0530

    Added term related configuration

--- init.el
+++ init.el
@@ -8,6 +8,22 @@
 
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 160)
 
+;; Kill term buffer on exit
+(defun oleh-term-exec-hook ()
+  (let* ((buff (current-buffer))
+         (proc (get-buffer-process buff)))
+    (set-process-sentinel
+     proc
+     `(lambda (process event)
+        (if (string= event "finished\n")
+            (kill-buffer ,buff))))))
+
+(add-hook 'term-exec-hook 'oleh-term-exec-hook)
+
+;; Map paste to C-c C-y
+(eval-after-load "term"
+  '(define-key term-raw-map (kbd "C-c C-y") 'term-paste))
+
 ;; Initialize package sources
 (require 'package)
 
